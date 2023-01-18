18f547beaa6384dcdb470aa1d1d2037ed09f0027
GentleMizt <olamideiyanda18@gmail.com>
2023-01-18 15:44:53 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 
 function App() {
   const [arrOfData, setArrOfData] =useState([]);
+  
+
 
   useEffect(() =>{
     fetch('https://jsonplaceholder.typicode.com/users').then(data =>{
       return data.json();
     }).then(result=>{
-      console.log(result);
+      // console.log(result);
       setArrOfData(result);
+      console.log(arrOfData);
     }).catch(err => console.log(err));
   }, [])
 
 
   return (
-  <p>Hello</p>
+    <div>
+      {arrOfData.map((cur, index)=> <p key={index}>{cur.name}</p>)}
+    </div>
   );
 }
 
