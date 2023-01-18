2eeea958680ff04719f68e6b7e7928d7d2e7679f
GentleMizt <olamideiyanda18@gmail.com>
2023-01-18 15:22:21 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [arrOfData, setArrOfData] =useState([]);
+
   useEffect(() =>{
     fetch('https://jsonplaceholder.typicode.com/users').then(data =>{
       return data.json();
     }).then(result=>{
       console.log(result);
+      setArrOfData(result);
     }).catch(err => console.log(err));
   }, [])
 
