2d5cd5897d0542cf4fac5e16c89877bd3956d07d
GentleMizt <olamideiyanda18@gmail.com>
2023-01-19 15:42:28 +0100

    progress

--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({cur}) => {
   return (
     <div className='card'>
-        <img/>
+        <img src={`https://robohash.org/2${cur.id}?size=150x150&set=set2`}/>
         <h2>{cur.name}</h2>
         <p>{cur.email}</p>
     </div>
